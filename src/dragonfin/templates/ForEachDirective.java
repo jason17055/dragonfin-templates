4632cdfbdbb4d3cc14bb5fb6f51c4198766dd538
Jason Long <jason@long.name>
2013-04-10 16:54:29 -0400

    FOREACH: ability to handle Java arrays, and Callable objects

--- src/dragonfin/templates/ForEachDirective.java
+++ src/dragonfin/templates/ForEachDirective.java
@@ -2,6 +2,7 @@ package dragonfin.templates;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.Callable;
 
 class ForEachDirective implements Directive
 {
@@ -16,29 +17,69 @@ class ForEachDirective implements Directive
 		this.content = content;
 	}
 
-	public void execute(Context ctx)
+	private void executeHelper(Context ctx, List<Object> asList)
 		throws IOException, TemplateRuntimeException
 	{
-		Object v = listExpr.evaluate(ctx);
+		@SuppressWarnings("unchecked")
+		Map<String,Object> varsMap = (Map<String,Object>)ctx.vars;
+		for (Object o : asList)
+		{
+			varsMap.put(loopVariableName, o);
+			content.execute(ctx);
+		}
+	}
+
+	void exHelper(Context ctx, Object v)
+		throws IOException, TemplateRuntimeException
+	{
+		if (v == null)
+		{
+			//same as an empty list
+			return;
+		}
+
 		if (v instanceof List)
 		{
 			@SuppressWarnings("unchecked")
 			List<Object> asList = (List<Object>) v;
-			@SuppressWarnings("unchecked")
-			Map<String,Object> varsMap = (Map<String,Object>)ctx.vars;
-			for (Object o : asList)
+
+			executeHelper(ctx, asList);
+			return;
+		}
+
+		if (v instanceof Object[])
+		{
+			// Note- this handles arrays of Object references only;
+			// primitive arrays will still fail
+			List<Object> asList = Arrays.asList((Object[])v);
+			executeHelper(ctx, asList);
+			return;
+		}
+
+		if (v instanceof Callable)
+		{
+			Callable<?> asCallable = (Callable<?>) v;
+			try
 			{
-				varsMap.put(loopVariableName, o);
-				content.execute(ctx);
+				Object v2 = asCallable.call();
+				if (v2 != v) {
+					exHelper(ctx, v2);
+					return;
+				}
+			}
+			catch (Exception e)
+			{
+				throw new TemplateRuntimeException("Exception thrown by "+v.getClass().getName()+".call() method", e);
 			}
 		}
-		else if (v == null)
-		{
-			//not an error
-		}
-		else
-		{
-			throw new TemplateRuntimeException("cannot iterate over non-list object");
-		}
+
+		throw new TemplateRuntimeException("cannot iterate over non-list object, "+v.getClass());
+	}
+
+	public void execute(Context ctx)
+		throws IOException, TemplateRuntimeException
+	{
+		Object v = listExpr.evaluate(ctx);
+		exHelper(ctx, v);
 	}
 }
