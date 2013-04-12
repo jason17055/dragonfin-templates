fc248a9a8ab94f0c28f8bae5afc4dcfaeeaf9150
Jason Long <jason@long.name>
2013-04-11 21:19:00 -0400

    refactor: move FunctionCall to separate file

--- src/dragonfin/templates/Parser.java
+++ src/dragonfin/templates/Parser.java
@@ -1055,25 +1055,6 @@ class Parser
 		return t.text;
 	}
 
-	static class FunctionCall extends Expression
-	{
-		String functionName;
-		List<Argument> arguments;
-
-		FunctionCall(String functionName, List<Argument> arguments)
-		{
-			this.functionName = functionName;
-			this.arguments = arguments;
-		}
-
-		@Override
-		Object evaluate(Context ctx)
-			throws TemplateRuntimeException
-		{
-			throw new TemplateRuntimeException("not implemented");
-		}
-	}
-
 	static class Variable extends Expression
 	{
 		String variableName;
